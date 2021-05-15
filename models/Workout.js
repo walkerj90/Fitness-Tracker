075eb8248d231653ad9ab091d6e154fb85666a38
Justin <walkerj90@icloud.com>
2021-05-15 10:29:00 -0700

    added new schema for workout and the properties with it

--- models/Workout.js
+++ models/Workout.js
@@ -2,3 +2,34 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
+  type: {
+    type: String,
+    enum: ["resistance", "cardio"],
+    required: "Valid options are 'resistance' or 'cardio'",
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: "Please enter a name for the exercise",
+  },
+  duration: {
+    type: Number,
+    required: "A duation is needed in minutes",
+  },
+  weight: {
+    type: Number,
+    required: isRequired("weight"),
+  },
+  reps: {
+    type: Number,
+    required: isRequired("reps"),
+  },
+  sets: {
+    type: Number,
+    required: isRequired("sets"),
+  },
+  distance: {
+    type: Number,
+    required: isRequired("distance"),
+  },
+});
